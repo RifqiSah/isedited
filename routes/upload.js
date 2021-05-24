69b2d3dc857f1719445c3b061f8e1a1e6ca38e63
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2021-05-24 13:58:14 +0700

    feat: image upload

--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.post('/', (req, res, next) => {
-  res.render('result', { content: "Berhasil upload!" });
+const path = require('path');
+const { unlinkSync } = require('fs');
+
+const { check } = require('../helpers/exif');
+const { random } = require('../helpers/helper');
+
+const cameraData = require('../data/camera.json');
+const softwareData = require('../data/software.json');
+
+const multer  = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, random());
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('image'), async (req, res, next) => {
+  const resp = {
+    content: "Berhasil upload!",
+    message: "",
+    data: "",
+  };
+
+  const imagePath = path.join(__dirname, `../${req.file.path}`);
+  const imageCheck = await check(imagePath).catch((err) => {
+    console.error(err);
+  });
+
+  unlinkSync(imagePath);
+
+  if (!imageCheck) {
+    resp.message = 'an error occured!';
+  }
+
+  if (!imageCheck.success) {
+    resp.message = imageCheck.message;
+  }
+
+  resp.data = JSON.stringify(imageCheck.data, null, 4);
+
+  res.render('result', resp);
 });
 
 module.exports = router;

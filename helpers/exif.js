70e2961feff76d19486c8c1ad0f21619388ad0fd
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2021-05-25 12:09:34 +0700

    refactor: exif helper

--- helpers/exif.js
+++ helpers/exif.js
@@ -1,11 +1,14 @@
 const ExifImage = require('exif').ExifImage;
 
-const check = (path) => {
+const cameraData = require('../data/camera.json');
+const softwareData = require('../data/software.json');
+
+const get = (path) => {
     return new Promise((resolve, reject) => {
         try {
             new ExifImage({ image: path }, function (error, exifData) {
                 if (error) {
-                    resolve({
+                    reject({
                         status: false,
                         data: null,
                         message: error.message,
@@ -29,4 +32,35 @@ const check = (path) => {
     });
 };
 
-module.exports = { check };
+const check = (exif) => {
+    const ret = {
+        edited: false,
+        message: "This image is original!",
+    };
+
+    // const cameraMatch = cameraData.some(s => {
+    //     if (exif.image.Make) {
+    //         return new RegExp(s).test(exif.image.Make);
+    //     }
+    // });
+
+    // if (cameraMatch) {
+    //     ret.edited = true;
+    //     ret.message = `This image is edited with ${exif.image.Model}!`;
+    // }
+
+    const softwareMatch = softwareData.some(s => {
+        if (exif.image.Software) {
+            return new RegExp(s).test(exif.image.Software);
+        }
+    });
+
+    if (softwareMatch) {
+        ret.edited = true;
+        ret.message = `This image is edited with <b>${exif.image.Software}</b>!`;
+    }
+
+    return ret;
+};
+
+module.exports = { get, check };

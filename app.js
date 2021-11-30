72c443f2e368c97699caea9a610ad33c3b29d8eb
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2021-11-30 19:37:09 +0700

    test: deployment

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
+
+app.listen(3000, () => {
+  console.log(`Server running at port ${3000}!`);
+});
